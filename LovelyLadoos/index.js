bcb159d642f72e5e8e6ef0429811a89ca320f734
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2020-12-06 02:44:30 +0100

    :sparkles: Challenge 2 solution

--- LovelyLadoos/index.js
+++ LovelyLadoos/index.js
@@ -1,13 +1,53 @@
+const predictionApi = require("@azure/cognitiveservices-customvision-prediction");
+const msRest = require("@azure/ms-rest-js");
+
+const predictor_credentials = new msRest.ApiKeyCredentials({
+  inHeader: { "Prediction-key": process.env.CUSTOM_VISION_KEY },
+});
+
+const predictor = new predictionApi.PredictionAPIClient(
+  predictor_credentials,
+  process.env.CUSTOM_VISION_ENDPOINT
+);
+
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+  const imageUrl = req.query.url || req.body.url;
 
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const response = {};
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+  if (imageUrl) {
+    try {
+      const result = await predictor.classifyImageUrl(
+        process.env.CUSTOM_VISION_PROJECT_ID,
+        "lovelyladoos",
+        {
+          url: imageUrl,
+        }
+      );
+
+      let prediction;
+      result.predictions.forEach((p) => {
+        if (!prediction || prediction.probability < p.probability) {
+          prediction = p;
+        }
+      });
+
+      response.body = {
+        prediction: prediction.tagName,
+        probability: prediction.probability,
+        details: result.predictions,
+      };
+    } catch (error) {
+      response.status = 400;
+      response.body = {
+        message: "An error has occured",
+        error: error.code,
+      };
+    }
+  } else {
+    response.status = 400;
+    response.body = "Please provide a valid image url.";
+  }
+
+  context.res = response;
 };
